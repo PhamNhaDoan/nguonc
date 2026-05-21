72ab7695f8e4c25f52a3e8ec317527c302abc24b
PhamNhaDoan <131529431+PhamNhaDoan@users.noreply.github.com>
2026-05-21 15:34:31 +0700

    Refactor index.js to use NguonCProvider class

--- index.js
+++ index.js
@@ -1,39 +1,56 @@
-const baseUrl = "https://phim.nguonc.com/api";
+class NguonCProvider extends DefaultProvider {
+    constructor() {
+        super({
+            name: "Nguồn C Phim",
+            baseUrl: "https://phim.nguonc.com/api",
+            lang: "vi",
+            type: ProviderType.Movie
+        });
+    }
 
-async function getHome() {
-    const res = await fetch(`${baseUrl}/films/phim-moi-cap-nhat?page=1`);
-    const json = await res.json();
-    return json.items.map(item => ({
-        name: item.name,
-        url: `${baseUrl}/film/${item.slug}`,
-        poster: item.thumb_url
-    }));
+    async getHome() {
+        const res = await this.request(`${this.baseUrl}/films/phim-moi-cap-nhat?page=1`);
+        const json = JSON.parse(res);
+        return json.items.map(item => ({
+            name: item.name,
+            url: `${this.baseUrl}/film/${item.slug}`,
+            poster: item.thumb_url
+        }));
+    }
+
+    async search(query) {
+        const res = await this.request(`${this.baseUrl}/films/search?keyword=${encodeURIComponent(query)}`);
+        const json = JSON.parse(res);
+        return json.items.map(item => ({
+            name: item.name,
+            url: `${this.baseUrl}/film/${item.slug}`,
+            poster: item.thumb_url
+        }));
+    }
+
+    async loadDetail(url) {
+        const res = await this.request(url);
+        const json = JSON.parse(res);
+        const movie = json.movie;
+
+        const episodes = json.episodes.flatMap(server => 
+            server.items.map(ep => ({
+                name: `Tập ${ep.name}`,
+                url: ep.embed,
+                description: movie.description
+            }))
+        );
+
+        return {
+            name: movie.name,
+            poster: movie.thumb_url,
+            description: movie.description,
+            episodes: episodes
+        };
+    }
 }
 
-async function search(query) {
-    const res = await fetch(`${baseUrl}/films/search?keyword=${encodeURIComponent(query)}`);
-    const json = await res.json();
-    return json.items.map(item => ({
-        name: item.name,
-        url: `${baseUrl}/film/${item.slug}`,
-        poster: item.thumb_url
-    }));
-}
-
-async function loadDetail(url) {
-    const res = await fetch(url);
-    const json = await res.json();
-    const movie = json.movie;
-    
-    const episodes = json.episodes.flatMap(server => 
-        server.items.map(ep => ({
-            name: `Tập ${ep.name}`,
-            url: ep.embed, // Hoặc ep.m3u8 tùy thuộc vào dữ liệu API trả về
-            description: movie.description
-        }))
-    );
-
-    return {
+registerProvider(new NguonCProvider());
         name: movie.name,
         poster: movie.thumb_url,
         description: movie.description,
